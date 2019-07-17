26471e5200f84eda2e9e6b9f9b1a32dad00d50c8
Gabriel <ggkpacker@gmail.com>
2019-07-17 16:12:11 -0300

    Add task list model

--- db/migrate/20190717185022_create_task_lists.rb
+++ db/migrate/20190717185022_create_task_lists.rb
@@ -1,9 +1,10 @@
 class CreateTaskLists < ActiveRecord::Migration[5.2]
   def change
     create_table :task_lists do |t|
-      t.string :title
-      t.boolean :public
-      t.boolean :favorite
+      t.string :title, null: false
+      t.boolean :open, default: false, null: false
+      t.boolean :favorite, default: false, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end

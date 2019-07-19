74eb8abca56c79a1e35a42656d042d82bb7521d4
Gabriel <ggkpacker@gmail.com>
2019-07-19 14:56:32 -0300

    Add task controller & routes

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TasksController < ApiController
+      before_action :set_task_list, only: %i[index create]
+      before_action :set_task, only: %i[show update destroy]
+
+      def index
+        @tasks = @task_list.tasks
+
+        render json: @tasks
+      end
+
+      def show
+        render json: @task
+      end
+
+      def create
+        @task = Task.new(task_params.merge(task_list: @task_list))
+
+        if @task.save
+          render json: @task
+        else
+          render json: { errors: @task.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @task.update(task_params)
+          render json: @task
+        else
+          render json: { errors: @task.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @task.destroy
+          head :ok
+        else
+          render json: { errors: @task.errors }, status: :conflict
+        end
+      end
+
+      def done
+        change_task_status(true)
+
+        render json: @task
+      end
+
+      def pending
+        change_task_status(false)
+
+        render json: @task.reload
+      end
+
+      private
+
+      def set_task_list
+        @task_list = TaskList.find(params[:task_list_id])
+      end
+
+      def set_task
+        @task = Task.find(params[:id])
+      end
+
+      def task_params
+        params.require(:task).permit(:title, :description, :done, :subtask)
+      end
+
+      def change_task_status(status)
+        @task.done = status
+
+        @task.save
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,16 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :task_lists, only: %i[index show create update destroy]
+      resources :task_lists, only: %i[index show create update destroy] do
+        resources :tasks, only: %i[index create]
+      end
+
+      resources :tasks, only: %i[show update destroy] do
+        member do
+          put 'done', to: 'tasks#done'
+          put 'pending', to: 'tasks#pending'
+        end
+      end
     end
   end
 end

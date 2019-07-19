4241d79b0da95998d33968575caea074c05875c0
Gabriel <ggkpacker@gmail.com>
2019-07-19 10:13:11 -0300

    Add missing password_confirmation on users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@email.com" }
-    password { 'secret' }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
